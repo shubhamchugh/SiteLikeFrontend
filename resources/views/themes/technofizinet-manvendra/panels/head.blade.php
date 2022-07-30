f6f94b745fd5e75c633e1f2b2c662b59cab85fed
Shubham chugh <chugh.shubham12@gmail.com>
2022-07-30 19:14:04 +0530

    head code settings added

--- resources/views/themes/technofizinet-manvendra/panels/head.blade.php
+++ resources/views/themes/technofizinet-manvendra/panels/head.blade.php
@@ -16,6 +16,10 @@
     <link rel="icon"
         href="https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://phpscripttr.com&size=32"
         type="image/png">
+
+    {{ nova_get_setting('head_code') }}
+
+
     <!-- MINIFIED -->
     {!! SEO::generate() !!}
 </head>
